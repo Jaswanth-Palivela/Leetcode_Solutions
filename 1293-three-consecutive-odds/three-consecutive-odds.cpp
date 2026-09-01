71df3cc3f6a79cb7940a23335dc3ee4f7dd42dc1
Jaswanth Palivela <palivelajaswanth263@gmail.com>
2026-09-01 10:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (89.25%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& a) {
+        for(int i=0;i+2<a.size();i++){
+            if(a[i]%2!=0 && a[i+1]%2!=0 && a[i+2]%2!=0){
+                return true;
+            }
+        }
+        return false;
+    }
+};
