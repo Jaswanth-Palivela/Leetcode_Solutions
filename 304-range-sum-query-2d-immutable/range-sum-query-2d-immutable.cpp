2c70d3aa36c32e1d805e77ebcf30fe3a01e1d9bc
Jaswanth Palivela <palivelajaswanth263@gmail.com>
2026-09-18 12:53:19 +0530

    Time: 27 ms (45.71%) | Memory: 149.3 MB (28.13%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -0,0 +1,15 @@
+class NumMatrix {
+public:
+    vector<vector<int>> p;
+    NumMatrix(vector<vector<int>>& a) {
+        int m = a.size(), n = a[0].size();
+        p.resize(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                p[i][j] = a[i - 1][j - 1] + p[i - 1][j] + p[i][j - 1] -
+                          p[i - 1][j - 1];
+    }
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        return p[r2 + 1][c2 + 1] - p[r1][c2 + 1] - p[r2 + 1][c1] + p[r1][c1];
+    }
+};
